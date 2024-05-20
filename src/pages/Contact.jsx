6ada7d7ced71b32268b55a0b6d983ad476193be5
haristomar45 <tomarh729@gmail.com>
2024-05-20 16:44:15 +0530

    second commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../component/Navbar";
+import { Link } from "react-router-dom";
 
 const Contact = () => {
   return (
@@ -8,7 +9,7 @@ const Contact = () => {
       <div className="bg-zinc-900  min-h-screen grid w-full  grid-cols-1 gap-8 mx-auto pt-8 md:grid-cols-3 lg:gap-16 px-8 pb-10 md:pb-0">
         <div className="block md:h-96 h-36 md:max-w-xs max-w-2xl bg-zinc-900 border border-zinc-400 rounded-lg shadow hover:bg-zinc-800/20  dark:hover:bg-zinc-500">
           <a
-            target="_blank"
+            target=""
             className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24 lg:pb-48 md:p-16 "
             href="">
             <span
@@ -54,11 +55,12 @@ const Contact = () => {
             </div>
           </a>
         </div>
+
         <div className="block md:h-96 h-36 md:max-w-xs max-w-2xl bg-zinc-900 border border-zinc-400 rounded-lg shadow hover:bg-zinc-800/20  dark:hover:bg-zinc-500">
           <a
-            target="_blank"
+            target=""
             className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24 lg:pb-48 md:p-16 "
-            href="">
+            href="https://github.com/harishtomar45">
             <span
               className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
               aria-hidden="true"></span>
@@ -86,28 +88,16 @@ const Contact = () => {
             </div>
           </a>
         </div>
+
         <div className="block md:h-96 h-36 md:max-w-xs max-w-2xl bg-zinc-900 border border-zinc-400 rounded-lg shadow hover:bg-zinc-800/20  dark:hover:bg-zinc-500">
           <a
-            target="_blank"
+            target=""
             className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24 lg:pb-48 md:p-16 "
-            href="">
+            href="https://linkedin.com/in/harish-tomar45">
             <span
               className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
               aria-hidden="true"></span>
             <span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
-              {/* <svg
-                aria-hidden="true"
-                focusable="false"
-                className="octicon octicon-mark-github"
-                viewBox="0 0 16 16"
-                width="20"
-                height="20"
-                fill="currentColor"
-                // style="display: inline-block; user-select: none; vertical-align: text-bottom; overflow: visible;"
-              >
-                <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
-              </svg> */}
-
               <svg
                 class="h-8 w-8 text-slate-100"
                 fill="none"
@@ -131,6 +121,7 @@ const Contact = () => {
             </div>
           </a>
         </div>
+
         {/* <div>GitHub</div>
             <div>link</div> */}
       </div>
